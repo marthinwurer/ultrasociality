486ae5d63ba34609a274d5b968c6ec417ba5c1a8
Benjamin Maitland <marthinwurer@gmail.com>
2018-02-13 15:16:50 -0500

    Added ice rendering, sea level change, and different noises

--- land.py
+++ land.py
@@ -4,9 +4,13 @@ from opensimplex import OpenSimplex
 import numpy as np
 import matplotlib.pyplot as plt
 import math as m
+from scipy.ndimage.filters import gaussian_filter
 
-height = 512
-width = 1024
+# height = 512
+# width = 1024
+height = 128
+width = 256
+sea_level = 0.65
 inclination = 1
 
 """
@@ -86,15 +90,17 @@ class NoiseWrapper():
             v = 2**e
             print(v)
             map += self.polar_fast_noise(h, w, v) / v
-            self.fast_noise = fns.Noise()
+            # self.fast_noise = fns.Noise()
 
         return normalize_map(map)
 
 
 
-def hillshade(array, azimuth, angle_altitude):
+def hillshade(array:np.ndarray, azimuth:int=315, angle_altitude:int=45):
     """ from
     http://geoexamples.blogspot.com/2014/03/shaded-relief-images-using-gdal-python.html
+
+    use values from http://www.shadedrelief.com/web_relief/
     Args:
         array:
         azimuth:
@@ -114,8 +120,42 @@ def hillshade(array, azimuth, angle_altitude):
                * np.cos(azimuthrad - aspect)
     return (shaded + 1) / 2
 
+def base_temp(h, w):
+    """
+    float base_temp(float latitude){
+	if(fabs(latitude) < .2){
+		return 27 + 273.15;
+	}
+	float val = fabs(latitude) * -67.5 + 40.5 + 273.15; // that kelvin tho
+	return val;
+}
+
+    Args:
+        h:
+        w:
+
+    Returns:
+
+    """
+    map = np.zeros((h, w))
+
+    for y in range(h):
+        latitude_percent = (1.0 - (y + .5) / (h / 2))
+
+        if abs(latitude_percent) < .2:
+            temp = 27 + 273.15
+        else:
+            temp = abs(latitude_percent) * -67.5 + 40.5 + 273.15
+        # print(latitude_percent * 90, temp - 273.15)
+
+        for x in range(w):
+            map[y][x] = temp
+    return map
+
+
 def main():
     n = NoiseWrapper()
+    n.fast_noise.seed = 1337
 
     # image = n.polar_fast_noise(height, width, 1)
     # plt.imshow(image)
@@ -128,11 +168,51 @@ def main():
 
     heightmap = n.get_normalized_world_fast(height, width, 6)
     # roughness_map = n.get_normalized_world_fast(height, width, 3)
+    heightmap = (heightmap - sea_level).clip(0) / sea_level
     heightmap **= 1.5
 
-    # base_temp_map = n.get_normalized_world(height, width, 10)
 
-    srm = normalize_map(hillshade(heightmap, 315, 45)) + .2
+    height_m = heightmap * 9000.
+    m_adibatic_change = height_m * -5e-3
+    base_temp_map = base_temp(height, width)
+    t = NoiseWrapper()
+    t.fast_noise.seed = 13337
+    temp_map = (t.get_normalized_world_fast(height, width, 3) - .5) * 10 + base_temp_map + m_adibatic_change
+
+    temp_map_c = temp_map - 273.15
+
+    m = NoiseWrapper()
+    m.fast_noise.seed = 133337
+
+    moisture_map = m.get_normalized_world_fast(height, width, 3) * 300. # cm/year
+
+
+
+
+
+    # changed = True
+    # min = 1.0
+    # while changed:
+    #     changed = False
+    #     for y in range(height):
+    #         for x in range(width):
+    #             if moisture_map[y][x] < min:
+    #                 changed = True
+    #                 north = moisture_map[y-1][x]
+    #                 south = moisture_map[(y+1)%height][x]
+    #                 east = moisture_map[y][(x+1)%width]
+    #                 west = moisture_map[y][x-1]
+    #                 moisture_map[y][x] = max([north, south, east, west]) - 1
+    #     moisture_map += 1
+
+    # plt.imshow(moisture_map)
+    # plt.show()
+
+
+
+
+
+    srm = normalize_map(hillshade(heightmap))
     # plt.imshow(srm)
     # plt.show()
     srm_image = np.reshape(srm, (height, width, 1))
@@ -144,10 +224,14 @@ def main():
             rv = 0
             gv = 0
             bv = 0
-            if heightmap[y][x] < .5:
-                bv = 1.0
+            if temp_map_c[y][x] < -10:
+                rv = .5
+                gv = .5
+                bv = 1.
+            elif heightmap[y][x] > 0.:
+                gv = heightmap[y][x] / 2 + .5
             else:
-                gv = heightmap[y][x]
+                bv = 1.0
 
 
             image[y][x][r] = rv
@@ -172,4 +256,5 @@ def fastnoisetest():
 
 if __name__=="__main__":
     # fastnoisetest()
+    # base_temp(height, width)
     main()
